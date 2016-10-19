c5ba487218b9420c3e97755fbf03358caa6e88d7
connor <connor.ferguson.offutt@gmail.com>
2016-10-19 13:43:53 -0400

    list checks for bananas and responds accordingly

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,9 @@
 grocery_list = ["* steaks", "* chicken", "* apples", "* bread", "* rice"]
   puts grocery_list
   puts grocery_list.length.to_s + " Items on grocery list"
-  
+
+if grocery_list.include?("bananas") == true
+    puts "You need to get some Bananas."
+  else
+    puts "You dont need any Bananas."
+  end
