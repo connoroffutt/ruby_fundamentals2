d821833b58e58aa476ead2d5e734e285c30cd8c6
connor <connor.ferguson.offutt@gmail.com>
2016-10-19 14:58:40 -0400

    increase class sizes by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -10,3 +10,10 @@ students[:cohort4] = 43
 students.each do |x, y|
   puts "The name of a Cohort is #{x}, the amount of students in this Cohort is #{y}"
 end
+
+students.each do |x, y|
+  increase = y * 1.05
+  students[x] = increase
+
+    puts "#{x} increased in size to #{increase}"
+end
