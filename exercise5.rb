d99de88200c63c8fc635bb9ff19f89df5af6a539
connor <connor.ferguson.offutt@gmail.com>
2016-10-19 13:03:02 -0400

    exercise 5 answer

--- exercise5.rb
+++ exercise5.rb
@@ -2,10 +2,4 @@
 fahrenheit = gets.chomp
 celsius = (fahrenheit.to_f - 32) * (5/9.to_f)
   puts "It is #{celsius} degrees Celsius outside."
-    if celsius < 15
-      puts "It\'s a bit nippy out here!"
-    elsif celsius > 15
-      puts "It\'s a quite nice out today isn\'t it?"
-    elsif celsius < 0
-      puts "Wow, it\'s FREEZING!"
-end
+    
